70a8fbb18f6543cf45cf3ed5709b8eb7d5ad4510
Stefano Ballirano <tafkas@CountDooku.local>
2015-11-09 13:49:17 +0100

    MyTest

--- monnezza5/TFKAppDelegate.h
+++ monnezza5/TFKAppDelegate.h
@@ -8,8 +8,15 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface TFKAppDelegate : NSObject <NSApplicationDelegate>
+@interface TFKAppDelegate : NSObject <NSApplicationDelegate, NSMenuDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property NSArray *runningApps;
+@property (weak) IBOutlet NSColorWell *colorApp;
+@property (weak) IBOutlet NSSlider *slider;
+@property (weak) IBOutlet NSTableView *myTableView;
+@property (weak) IBOutlet NSMenu *myMenu;
+@property  NSWindow *myWindow;
+- (IBAction)openWindow:(id)sender;
 
 @end
--- monnezza5/TFKAppDelegate.m
+++ monnezza5/TFKAppDelegate.m
@@ -10,9 +10,66 @@
 
 @implementation TFKAppDelegate
 
+NSWindow *myWindow2;
+NSInteger rigaSelezionata =0;
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    self.runningApps = [NSMutableArray arrayWithArray:[[NSWorkspace sharedWorkspace] runningApplications]];
+    NSInteger selez = [self.myTableView selectedRow];
+    NSLog(@"%ld", (long)selez);
+    [self.myTableView deselectRow:selez];
+    
+    
+    
+    
+    
 }
 
+
+- (void)tableViewSelectionDidChange:(NSNotification *)aNotification{
+    if ([self.myTableView selectedRow] == -1) {
+        return;
+    }
+    rigaSelezionata = [self.myTableView selectedRow];
+    
+    [[self.runningApps objectAtIndex:rigaSelezionata] setValue:[[[self.runningApps objectAtIndex:rigaSelezionata] valueForKey:@"localizedName"] uppercaseString] forKey:@"localizedName"] ;
+
+}
+
+-(void) changeColor{
+    [self.colorApp setColor:[NSColor greenColor]];
+    
+}
+
+
+- (void) menuWillOpen:(NSMenu *)menu{
+    if ([[menu title] isEqualToString:@"Show Fonts"]) {
+        NSLog(@"%@", menu);
+    }
+    
+    NSLog(@"%@", menu);
+}
+- (IBAction)openWindow:(id)sender {
+    
+   myWindow2 = [[NSWindow alloc] initWithContentRect:NSMakeRect(50, 50, 500, 200) styleMask:(NSTitledWindowMask | NSMiniaturizableWindowMask | NSResizableWindowMask | NSClosableWindowMask) backing:NSBackingStoreBuffered defer:NO];
+    [myWindow2 setReleasedWhenClosed:YES];
+    self.myWindow = myWindow2;
+    [self.myWindow setDelegate:(id)self];
+    NSButton *myButton = [[NSButton alloc] initWithFrame:NSMakeRect(100, 100, 120, 50)];
+    [myButton setTitle:@"Fottiti"];
+    [myButton setButtonType:NSMomentaryLightButton]; //Set what type button You want
+    [myButton setBezelStyle:NSRoundedBezelStyle]; //Set what style You want
+    [myButton setAction:@selector(changeColor)];
+    [[self.myWindow contentView] addSubview:myButton];
+    [self.myWindow makeKeyAndOrderFront:NSApp];
+    
+
+}
+
+- (void)windowWillClose:(NSNotification *)notification{
+    NSLog(@"Po");
+    myWindow2 = nil;
+    
+}
 @end
